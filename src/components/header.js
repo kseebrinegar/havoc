f1973cae6646a1def98c7a12b1156b6efcc384c8
kseebrinegar <ksee.brinegar@hotmail.com>
2018-02-10 12:41:40 -0800

    init

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="hamburgerLogo">
+			<p className="hamburgerLogo__letter">H</p>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/routes/route.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../components/home';
+import Careers from '../components/careers';
+import Contact from '../components/contact';
+import Work from '../components/work';
+import About from '../components/about';
+import Header from '../components/header';
+
+class AppRouter extends React.Component {
+	render() {
+		
+		return (
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Switch>
+						<Route path="/" component={Home} exact={true} />
+						<Route path="/careers" component={Careers} />
+						<Route path="/about" component={About} />
+						<Route path="/work" component={Work} />
+						<Route path="/contact" component={Contact} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
+};
+
+export default AppRouter;
