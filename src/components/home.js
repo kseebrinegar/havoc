1ffa259fe0ec35ae2fcad820c034aff5a9397b00
kseebrinegar <ksee.brinegar@hotmail.com>
2018-02-23 14:10:54 -0800

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -228,8 +228,8 @@ class Home extends Component {
         });*/
 	}
     render() {
-        var videoHeight = (window.innerHeight  / 100) * 32;
-        
+        //let videoHeight = window.innerWidth <= 740 ? (window.innerHeight  / 100) * 32 + 'px' : '100%';
+        let videoHeight = '40%';
         return (
             <div className="home" style={{top: this.state.homeScrollDown + 'px'}}>
                 <div id="swipe-container-mobile">
@@ -250,7 +250,7 @@ class Home extends Component {
                         		<p className="home__section--slide-show__content__border-line"></p>
                         		<p>America's leading legware brand No Nonesense names Havoc 
                         		Agency of Record.</p>
-                                <div className="video" style={{height: videoHeight + 'px'}}>
+                                <div className="video" style={{height: videoHeight}}>
                                     <Player fluid={true} height={800} width={800} playsInline poster="/assets/poster.png" src="//videos.contentful.com/fiz3jwws2um7/201F4YQCtmEIMm4eMyqkI0/929be99e38e63bccd184881c485f752e/SuperBowlNoText.mov" />
                                 </div> 
                             </div>
