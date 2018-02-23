701c23eab0fb10b9df6f1a24e4d2a9549863607a
kseebrinegar <ksee.brinegar@hotmail.com>
2018-02-23 14:37:36 -0800

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -251,7 +251,7 @@ class Home extends Component {
                         		<p>America's leading legware brand No Nonesense names Havoc 
                         		Agency of Record.</p>
                                 <div className="video" style={{height: videoHeight}}>
-                                    <Player fluid={true} height={800} width={800} playsInline poster="/assets/poster.png" src="//videos.contentful.com/fiz3jwws2um7/201F4YQCtmEIMm4eMyqkI0/929be99e38e63bccd184881c485f752e/SuperBowlNoText.mov" />
+                                    <Player playsInline poster="/assets/poster.png" src="//videos.contentful.com/fiz3jwws2um7/201F4YQCtmEIMm4eMyqkI0/929be99e38e63bccd184881c485f752e/SuperBowlNoText.mov" />
                                 </div> 
                             </div>
                              <div className="home__section--slide-show__content--container2" style={{height: this.state.windowHeight}}>
@@ -267,8 +267,8 @@ class Home extends Component {
                                 <p className="home__section--slide-show__content__border-line"></p>
                                 <p>America's leading legware brand No Nonesense names Havoc 
                                 Agency of Record</p>
-                                <div className="video">
-                                    <Player fluid={true} height={400} width={400} playsInline poster="/assets/poster.png" src="//videos.contentful.com/fiz3jwws2um7/201F4YQCtmEIMm4eMyqkI0/929be99e38e63bccd184881c485f752e/SuperBowlNoText.mov" />
+                                <div className="video" style={{height: videoHeight}}>
+                                    <Player playsInline poster="/assets/poster.png" src="//videos.contentful.com/fiz3jwws2um7/201F4YQCtmEIMm4eMyqkI0/929be99e38e63bccd184881c485f752e/SuperBowlNoText.mov" />
                                 </div> 
                             </div>
                              <div className="home__section--slide-show__content--container4" style={{height: this.state.windowHeight}}>
